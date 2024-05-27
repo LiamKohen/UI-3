b7d07849202eb82468e3b9c22712bc4466eb7869
LiamKohen <liamkohen@icloud.com>
2024-05-27 09:05:55 -0300

    LoginManager and Password

--- Assets/LoginManager.cs
+++ Assets/LoginManager.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class LoginManager : MonoBehaviour
 {
+
+    public InputField inputPassword;
+    public string contraCorrecta = "PepeCrack";
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,18 @@ public class LoginManager : MonoBehaviour
     void Update()
     {
         
+    }
+    public void CheckPassword()
+    {
+        if(inputPassword.text == contraCorrecta)
+        {
+            Debug.Log("Acces Granted");
+
+           
+        }
+        else
+        {
+            Debug.Log("Acces denied");
+        }
     }
 }
